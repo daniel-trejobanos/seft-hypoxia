7e2a2eeff1b9c964abe3c94de7fc5737cd1e4396
daniel-trejobanos <daniel.trejobanos@uzh.ch>
2021-02-15 08:43:53 +0100

    Revert "created fork to run seft with ICU-cockpit hypoxia"
    This reverts commit 0306c4cb82b0bcede3cc305bc31ed77bdbbfc8e0.

--- seft/tasks.py
+++ seft/tasks.py
@@ -221,8 +221,5 @@ DATASET_TO_TASK_MAPPING = {
         class_weights={0: 0.5553, 1: 5.0188}
     ),
     'mimic3_mortality': BinaryClassification(),
-    'mimic3_phenotyping': MultilabelClassification(25),
-    'icuc_hypoxia':OnlineBinaryClassification(
-        class_weights={0: 0.5553, 1: 5.0188}
-    )
+    'mimic3_phenotyping': MultilabelClassification(25)
 }
